f9de22f0528d0d646ec09eee98c098ae5d49e394
Robyn Smith <robyn.roybin@gmail.com>
2023-05-15 18:00:02 +0000

    Added testing and NotFound, and cleaned up errors

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -2,6 +2,7 @@ import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useHistory } from "react-router";
+import { removeTokenTimestamp, shouldRefreshToken } from "../utils/utils";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
@@ -13,42 +14,36 @@ export const CurrentUserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
     const history = useHistory();
 
-    // const handleMount = async () => {
-    //     try {
-    //         const { data } = await axiosRes.get("dj-rest-auth/user/");
-    //         setCurrentUser(data);
-    //     } catch (err) {
-    //         console.log(err);
-    //     }
-    // };
-
-    useEffect(() => {
-        const handleMount = async () => {
-          try {
-            const { data } = await axiosRes.get("dj-rest-auth/user/");
-            setCurrentUser(data);
-          } catch (err) {
-            console.log(err);
-          }
-        };
-      
+    const handleMount = async () => {
+        try {
+          const { data } = await axiosRes.get("dj-rest-auth/user/");
+          setCurrentUser(data);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+    
+      useEffect(() => {
         handleMount();
-      }, [setCurrentUser, history]);
+      }, []);
 
     useMemo(() => {
         axiosReq.interceptors.request.use(
             async (config) => {
-                try {
-                    await axios.post('/dj-rest-auth/token/refresh/')
-                } catch(err) {
-                    setCurrentUser((prevCurrentUser) => {
-                        if (prevCurrentUser) {
-                            history.push('signin')
-                        }
-                        return null
-                    })
-                    return config
+                if (shouldRefreshToken()) {
+                    try {
+                        await axios.post('/dj-rest-auth/token/refresh/')
+                    } catch(err) {
+                        setCurrentUser((prevCurrentUser) => {
+                            if (prevCurrentUser) {
+                                history.push('signin')
+                            }
+                            return null;
+                        })
+                        return config
+                    }
                 }
+                removeTokenTimestamp();
                 return config
             }, 
             (err) => {
@@ -67,10 +62,11 @@ export const CurrentUserProvider = ({ children }) => {
                             if (prevCurrentUser) {
                                 history.push('/signin')
                             }
-                            return null
-                        })
+                            return null;
+                        });
+                        removeTokenTimestamp();
                     }
-                    return axios(err.config)
+                    return axios(err.config);
                 }
                 return Promise.reject(err)
             }
---
+++ src/mocks/handlers.js
@@ -0,0 +1,22 @@
+import { rest } from "msw";
+
+const baseURL = "https://drf-api-roybin0.herokuapp.com/";
+
+export const handlers = [
+    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+        return res(
+            ctx.json({
+                "pk": 1,
+                "username": "sarah",
+                "email": "",
+                "first_name": "",
+                "last_name": "",
+                "profile_id": 1,
+                "profile_image": "https://res.cloudinary.com/dzo1ed9bg/image/upload/v1/media/../default_profile_cjqose"
+            })
+        );
+    }),
+    rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+        return res(ctx.status(200));
+    })
+];
